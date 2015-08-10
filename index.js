523bb3286904b4ee8b6d956d3500b99ad212093e
andrepolischuk <andre.polischuk@gmail.com>
2015-08-11 02:42:09 +0300

    Add support for not blocking rules, fix #1

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 export default (defaults = {}) => {
   const rules = [];
+  let blocking = true;
+  exec.blocking = useBlocking;
+  exec.notBlocking = useNotBlocking;
   exec.exec = exec;
   exec.use = use;
   return exec;
+
+  function useBlocking() {
+    blocking = true;
+    return exec;
+  }
+
+  function useNotBlocking() {
+    blocking = false;
+    return exec;
+  }
 
   function exec(value, fn, opts) {
     const errors = [];
     let i = 0;
 
     function next(res) {
-      const {ctx} = rules[i - 1] || {};
+      const {ctx, blocking} = rules[i - 1] || {};
       const {mw} = rules[i++] || {};
-
-      if (ctx && isResultInvalid(res)) {
-        errors.push(ctx);
-        return pushResult(errors, fn);
-      }
-
-      if (!mw) return pushResult(null, fn);
+      const err = ctx && isInvalid(res);
+      if (err) errors.push(ctx);
+      if (err && blocking) return pushResult(errors, fn);
+      if (!mw) return pushResult(errors, fn);
       res = mw(value, fn ? next : fn, opts);
       if (res !== undefined) return next(res);
     }
@@ -26,15 +36,16 @@ export default (defaults = {}) => {
   }
 
   function use(mw, ctx) {
-    rules.push({mw, ctx});
+    rules.push({mw, ctx, blocking});
     return exec;
   }
 };
 
-function isResultInvalid(res) {
+function isInvalid(res) {
   return res === false || Array.isArray(res);
 }
 
 function pushResult(res, fn) {
+  res = res.length ? res : null;
   return fn ? fn(res) : res;
 }
--- test.js
+++ test.js
@@ -7,88 +7,153 @@ const syncValidateStringType = validate()
 const syncValidateStringStart = validate()
   .use(value => value.charAt(0) === 'a', 'start');
 
+const syncValidateStringEnd = value => value.charAt(value.length - 1) === 'e';
+
 const syncValidateString = validate()
   .use(syncValidateStringType, 'type')
-  .use(syncValidateStringStart, 'start');
+  .use(syncValidateStringStart, 'start')
+  .use(syncValidateStringEnd, 'end');
 
-it('should pass sync validate', () => {
-  equal(syncValidateStringType('awesome'), null);
-});
-
-it('should fail sync validate', () => {
-  deepEqual(syncValidateStringType(12), ['type']);
-});
-
-it('should pass sync validate using function as middleware', () => {
-  equal(syncValidateString('awesome'), null);
-});
-
-it('should fail sync validate using function as middleware', () => {
-  deepEqual(syncValidateString(12), ['type']);
-  deepEqual(syncValidateString('superb'), ['start']);
-});
+const syncNotBlockingValidateString = validate()
+  .use(syncValidateStringType, 'type')
+  .notBlocking()
+  .use(syncValidateStringStart, 'start')
+  .use(syncValidateStringEnd, 'end');
 
 const asyncValidateStringType = validate()
   .use((value, next) => {
     setTimeout(() => {
       next(typeof value === 'string');
-    }, 500);
+    }, 100);
   }, 'type');
 
 const asyncValidateStringStart = validate()
   .use((value, next) => {
     setTimeout(() => {
       next(value.charAt(0) === 'a');
-    }, 500);
+    }, 100);
   }, 'start');
+
+const asyncValidateStringEnd = (value, next) => {
+  setTimeout(() => {
+    next(value.charAt(value.length - 1) === 'e');
+  }, 100);
+}
 
 const asyncValidateString = validate()
   .use(asyncValidateStringType, 'type')
-  .use(asyncValidateStringStart, 'start');
+  .use(asyncValidateStringStart, 'start')
+  .use(asyncValidateStringEnd, 'end');
 
-it('should pass async validate', (done) => {
+const asyncNotBlockingValidateString = validate()
+  .use(asyncValidateStringType, 'type')
+  .notBlocking()
+  .use(asyncValidateStringStart, 'start')
+  .use(asyncValidateStringEnd, 'end');
+
+const mixValidateString = validate()
+  .use(syncValidateStringType, 'type')
+  .use(asyncValidateStringStart, 'start')
+  .use(syncValidateStringEnd, 'end');
+
+const mixNotBlockingValidateString = validate()
+  .use(syncValidateStringType, 'type')
+  .notBlocking()
+  .use(asyncValidateStringStart, 'start')
+  .use(syncValidateStringEnd, 'end');
+
+it('should pass sync validation', () => {
+  equal(syncValidateStringType('awesome'), null);
+});
+
+it('should fail sync validation', () => {
+  deepEqual(syncValidateStringType(12), ['type']);
+});
+
+it('should pass sync validation using function as middleware', () => {
+  equal(syncValidateString('awesome'), null);
+});
+
+it('should fail sync validation using function as middleware', () => {
+  deepEqual(syncValidateString(12), ['type']);
+  deepEqual(syncValidateString('superb'), ['start']);
+});
+
+it('should pass sync validation using not blocking middleware', () => {
+  equal(syncNotBlockingValidateString('awesome'), null);
+});
+
+it('should fail sync validation using not blocking middleware', () => {
+  deepEqual(syncNotBlockingValidateString(12), ['type']);
+  deepEqual(syncNotBlockingValidateString('superb'), ['start', 'end']);
+});
+
+it('should pass async validation', (done) => {
   asyncValidateStringType('awesome', err => {
     equal(err, null);
     done();
   });
 });
 
-it('should fail async validate', (done) => {
+it('should fail async validation', (done) => {
   asyncValidateStringType(12, err => {
     deepEqual(err, ['type']);
     done();
   });
 });
 
-it('should pass async validate using function as middleware', (done) => {
+it('should pass async validation using function as middleware', (done) => {
   asyncValidateString('awesome', err => {
     equal(err, null);
     done();
   });
 });
 
-it('should fail async validate using function as middleware', (done) => {
+it('should fail async validation using function as middleware', (done) => {
   asyncValidateString('superb', err => {
     deepEqual(err, ['start']);
     done();
   });
 });
 
-const mixValidateString = validate()
-  .use(syncValidateStringType, 'type')
-  .use(asyncValidateStringStart, 'start')
-  .use(value => value.charAt(value.length - 1) === 'e' , 'end');
+it('should pass async validation using not blocking middleware', (done) => {
+  asyncNotBlockingValidateString('awesome', err => {
+    equal(err, null);
+    done();
+  });
+});
 
-it('should pass mix validate using function as middleware', (done) => {
+it('should fail async validation using not blocking middleware', (done) => {
+  asyncNotBlockingValidateString('superb', err => {
+    deepEqual(err, ['start', 'end']);
+    done();
+  });
+});
+
+it('should pass mix validation using function as middleware', (done) => {
   mixValidateString('awesome', err => {
     equal(err, null);
     done();
   });
 });
 
-it('should fail mix validate using function as middleware', (done) => {
+it('should fail mix validation using function as middleware', (done) => {
   mixValidateString('awesomeness', err => {
     deepEqual(err, ['end']);
+    done();
+  });
+});
+
+it('should pass mix validation using not blocking middleware', (done) => {
+  mixNotBlockingValidateString('awesome', err => {
+    equal(err, null);
+    done();
+  });
+});
+
+it('should fail mix validation using not blocking middleware', (done) => {
+  mixNotBlockingValidateString('superb', err => {
+    deepEqual(err, ['start', 'end']);
     done();
   });
 });
